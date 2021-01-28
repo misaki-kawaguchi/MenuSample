7d43f9b997589241d614546300230472ef92ee65
川口美咲 <misaki@arcphilia.co.jp>
2021-01-28 20:01:08 +0900

    [Add]定食メニューを生成する処理を追加

--- app/src/main/java/com/misakikawaguchi/menusample/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/menusample/MainActivity.kt
@@ -8,4 +8,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // 定食メニューを生成
+    private fun createTeishoku() : MutableList<MutableMap<String, Any>> {
+        val menuList : MutableList<MutableMap<String, Any>> = mutableListOf()
+
+        var menu = mutableMapOf("name" to "から揚げ定食", "price" to 800, "desc" to "から揚げ定食です")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ハンバーグ定食", "price" to 850, "desc" to "ハンバーグ定食です")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "生姜焼き定食", "price" to 800, "desc" to "生姜焼き定食です")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ステーキ定食", "price" to 1000, "desc" to "ステーキ定食です")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "野菜炒め定食", "price" to 750, "desc" to "野菜炒め定食です")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "とんかつ定食", "price" to 800, "desc" to "とんかつ定食です")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ミンチカツ定食", "price" to 850, "desc" to "ミンチカツ定食です")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "チキンカツ定食", "price" to 800, "desc" to "チキンカツ定食です")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "コロッケ定食", "price" to 750, "desc" to "コロッケ定食です")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "焼き魚定食", "price" to 800, "desc" to "焼き魚定食です")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "焼肉定食", "price" to 900, "desc" to "焼肉定食です")
+        menuList.add(menu)
+
+        return menuList
+    }
 }
